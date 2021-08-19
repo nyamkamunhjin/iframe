ab8689d3560ebd84bc3d3b84253b79be958cadcd
Munkhjin Nyamdorj <nyamkamunhjin@gmail.com>
2021-08-19 18:42:57 +0800

    init

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const app = express();
+const path = require('path');
+const { nextTick } = require('process');
+const port = 1337;
+
+app.use(cookieParser());
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  const cookie = req.cookies.cloudmnCookie;
+
+  if (!cookie) {
+    res.cookie('cloudmnCookie', 'hello', {
+      domain: 'ngrok.io',
+      maxAge: 900000,
+    });
+    console.log('cookie created successfully');
+  } else {
+    console.log('cookie exists', cookie);
+  }
+
+  console.log(req.get('Referer'));
+
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'iframe/button.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
